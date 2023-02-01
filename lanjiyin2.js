854ac43a10f2844b63d4e035db785703114f72c3
beichengyanyv <111874678+beichengyanyv@users.noreply.github.com>
2023-02-01 18:23:40 +0800

    Add files via upload

---
+++ lanjiyin2.js
@@ -0,0 +1,4 @@
+Hostname = *.lanjiyin.com.cn
+
+*.lanjiyin.com.cn(\/user\/account\/info|\/activity\/activity\/account) url script-response-body lanjiyin.js
+https://github.com/beichengyanyv/-/blob/d4769d4517645210485aaf1b7c80a7776d0d90f0/lanjiyin.js
